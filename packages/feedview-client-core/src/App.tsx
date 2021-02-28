3c2ebd2c502c83f80cd078207dc9b368653b2853
postfm <postfm@yandex.ru>
2021-02-28 19:57:25 +0300

    Add sliders

--- packages/feedview-client-core/src/App.tsx
+++ packages/feedview-client-core/src/App.tsx
@@ -1,18 +1,21 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import {
   Typography,
   Box,
   ThemeProvider,
   CssBaseline,
   createMuiTheme,
-} from '@material-ui/core';
-import useStore from './core/useStore';
-import Card from './components/NewsCard';
-import { compareDesc } from 'date-fns';
-import TweetCard from './components/TweetCard';
-import GlobalStats from './components/GlobalStats';
-import Header from './components/Header';
+} from "@material-ui/core";
+import useStore from "./core/useStore";
+import Card from "./components/NewsCard";
+import { compareDesc } from "date-fns";
+import TweetCard from "./components/TweetCard";
+import GlobalStats from "./components/GlobalStats";
+import Header from "./components/Header";
+import Flickity from "react-flickity-component";
+
+import "flickity/css/flickity.css";
 
 const theme = createMuiTheme({});
 
@@ -22,46 +25,71 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <div className='app'>
+      <div className="app">
         <Header />
-        <div className='main'>
-          <div className='content'>
-            <div>
-              <Box mb={2} display='block'>
-                <Typography variant='h5'>News articles</Typography>
-              </Box>
-              <div className='newsContainer'>
-                {news
-                  .sort((a, b) =>
-                    compareDesc(
-                      new Date(a.publishedAt),
-                      new Date(b.publishedAt),
-                    ),
-                  )
-                  .slice(0, 5)
-                  .map((item) => (
-                    <Card newsItem={item} key={item.url} />
-                  ))}
+        <div className="main">
+          <div className="content container">
+            <div className="info-post">
+              <div className="newsContainer">
+                <Box mb={2} display="block">
+                  <Typography variant="h5">News articles</Typography>
+                </Box>
+                <div className="postContainer">
+                  <Flickity
+                    options={{
+                      groupCells: true,
+                      wrapAround: true,
+                      autoPlay: true,
+                    }} // takes flickity options {}
+                    disableImagesLoaded={false} // default false
+                  >
+                    {news
+                      .sort((a, b) =>
+                        compareDesc(
+                          new Date(a.publishedAt),
+                          new Date(b.publishedAt)
+                        )
+                      )
+                      .slice(0, 5)
+                      .map((item) => (
+                        <div className="flickity-slide">
+                          <Card newsItem={item} key={item.url} />
+                        </div>
+                      ))}
+                  </Flickity>
+                </div>
+              </div>
+              <div className="tweetContainer">
+                <Box mb={2}>
+                  <Typography variant="h5">Twitter</Typography>
+                </Box>
+                <div className="newsContainer">
+                  <Flickity
+                    options={{
+                      groupCells: true,
+                      wrapAround: true,
+                      autoPlay: true,
+                    }} // takes flickity options {}
+                    disableImagesLoaded={false} // default false
+                  >
+                    {tweets
+                      .sort((a, b) =>
+                        compareDesc(
+                          new Date(a.created_at),
+                          new Date(b.created_at)
+                        )
+                      )
+                      .slice(0, 10)
+                      .map((item) => (
+                        <TweetCard tweet={item} key={item.id} />
+                      ))}
+                  </Flickity>
+                </div>
               </div>
             </div>
-            <div className='tweetContainer'>
+            <div className="globalStatsContainer">
               <Box mb={2}>
-                <Typography variant='h5'>Twitter</Typography>
-              </Box>
-              <div className='newsContainer'>
-                {tweets
-                  .sort((a, b) =>
-                    compareDesc(new Date(a.created_at), new Date(b.created_at)),
-                  )
-                  .slice(0, 10)
-                  .map((item) => (
-                    <TweetCard tweet={item} key={item.id} />
-                  ))}
-              </div>
-            </div>
-            <div className='globalStatsContainer'>
-              <Box mb={2}>
-                <Typography variant='h5'>Global stats</Typography>
+                <Typography variant="h5">Global stats</Typography>
               </Box>
               <GlobalStats articles={news} tweets={tweets} />
             </div>
--- packages/feedview-client-core/src/components/Header.tsx
+++ packages/feedview-client-core/src/components/Header.tsx
@@ -1,8 +1,8 @@
-import React, { SyntheticEvent, useEffect, useState } from 'react';
-import { Box, Button, TextField, Typography } from '@material-ui/core';
-import DynamicFeedIcon from '@material-ui/icons/DynamicFeed';
-import useStore from '../core/useStore';
-import useSocket from '../core/useSocket';
+import React, { SyntheticEvent, useEffect, useState } from "react";
+import { Box, Button, TextField, Typography } from "@material-ui/core";
+import DynamicFeedIcon from "@material-ui/icons/DynamicFeed";
+import useStore from "../core/useStore";
+import useSocket from "../core/useSocket";
 
 function Header() {
   const { sendQuery } = useSocket();
@@ -15,7 +15,7 @@ function Header() {
     }
   }, [getQuery]);
 
-  const [value, setValue] = useState('');
+  const [value, setValue] = useState("");
 
   function handleSubmit(event: SyntheticEvent) {
     event.preventDefault();
@@ -24,35 +24,35 @@ function Header() {
   }
 
   function handleClear() {
-    setQuery('');
-    setValue('');
+    setQuery("");
+    setValue("");
   }
 
   return (
-    <Box className='header'>
-      <Box className='logo'>
-        <DynamicFeedIcon fontSize='large' />
-        <Typography variant='h4' style={{ fontWeight: 600 }}>
+    <Box className="header container">
+      <Box className="logo">
+        <DynamicFeedIcon fontSize="large" />
+        <Typography variant="h4" style={{ fontWeight: 600 }}>
           Feedview
         </Typography>
       </Box>
       <Box>
-        <form className='form' onSubmit={handleSubmit}>
+        <form className="form" onSubmit={handleSubmit}>
           <TextField
             value={value}
             onChange={(e) => setValue(e.target?.value as string)}
           />
           <Button
-            variant='contained'
-            color='primary'
+            variant="contained"
+            color="primary"
             disableElevation
             onClick={handleSubmit}
           >
             Search
           </Button>
           <Button
-            variant='contained'
-            color='primary'
+            variant="contained"
+            color="primary"
             disableElevation
             onClick={handleClear}
           >
